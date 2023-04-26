cc2ec09f87167711fc880f3d3d73ab1a519ae173
Teddy Derkert <tderkert@users.noreply.github.com>
2023-04-26 22:17:28 +0200

    Fix bug
    Fixing a bug where the click events happen on child elements. Child elements lacked the `data-path` attribute which the wrapper element has. By preventing pointer events with css the clicks happen on the wrapper element instead.

--- src/components/modalDirectorySelector.svelte
+++ src/components/modalDirectorySelector.svelte
@@ -51,6 +51,9 @@
                 >
                 <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
                 {directory.directory_name}
+                <svg class="pointer-events-none" viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+                <span class="pointer-events-none w-full">{directory.directory_name}</span>
+                <span class="pointer-events-none opacity-40">{index+1}</span>
             </div>
         {/each}
     </div>
