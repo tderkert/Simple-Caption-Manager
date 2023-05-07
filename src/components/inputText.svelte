6b973d2416417bfb1903f2e1cec7ecf5f72f72f2
Teddy Derkert <tderkert@users.noreply.github.com>
2023-05-07 14:40:49 +0200

    Create Input component

---
+++ src/components/inputText.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let placeholder = "";
+    export let value = "";
+    export let id = "";
+</script>
+<input bind:value="{value}" class="px-3 py-2 rounded-xl bg-slate-900 ring-blue-500 focus:ring-2 transition placeholder-white placeholder-opacity-40" type="text" id={id} placeholder={placeholder}>
