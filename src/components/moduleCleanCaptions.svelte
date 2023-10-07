0dc321f1bd67b375b3f50379eef9986bd1203241
Teddy Derkert <tderkert@users.noreply.github.com>
2023-10-07 21:09:10 +0200

    Simplify cleanCaptions module

--- src/components/moduleCleanCaptions.svelte
+++ src/components/moduleCleanCaptions.svelte
@@ -1,23 +1,12 @@
 <script>
     import Button from '/src/components/button.svelte';
-    import { pairsStore, cleanCaptions } from '/src/lib/store/GlobalStore.js';
-
-    let pairsData = [];
-    let cleanPlaceholder = "Clean Captions";
-
-    pairsStore.subscribe(value => {
-        pairsData = value;
-    });
-
-    function handleCleanCaptions(){
-        cleanCaptions()
-    }
+    import { cleanCaptions } from '/src/lib/store/GlobalStore.js';
 </script>
 
 
 <div class="p-4 flex flex-col gap-4 items-stretch">
     <!-- Tidy Captions -->
     <form class="flex flex-col gap-4 items-stretch">
-        <Button on:click={ handleCleanCaptions }> Tidy Captions</Button>
+        <Button on:click={ cleanCaptions }> Tidy Captions</Button>
     </form>
 </div>
